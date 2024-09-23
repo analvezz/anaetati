0c8fdb89ca5e15dee0bee222551448c17f1e57c1
analvezz <anaclarapreta2@gmail.com>
2024-09-23 16:19:00 -0300

    Add files via upload

---
+++ static/js/perguntas.js
@@ -0,0 +1,30 @@
+// Se já houver uma variável 'palavras', você pode mudar para algo diferente
+const palavras= [
+
+    "hazel grace",
+    "augustus waters",
+    "osteossarcoma",
+    "peter van houten",
+    "amsterdã",
+    "cigarro",
+    "isaac",
+    "uma aflição imperial",
+    "coração literal",
+    "casa de anne frank",
+
+];
+
+const dicas= [
+
+    "protagonista do livro.",
+    "personagem masculino que hazel conhece no grupo de apoio.",
+    "tipo de câncer que augustus tem.",
+    "autor fictício do livro favorito de hazel.",
+    "cidade onde hazel e augustus viajam para encontrar o autor.",
+    "objeto que augustus sempre tem na boca, mas não usa.",
+    "melhor amigo de augustus que perde a visão.",
+    "livro fictício que hazel lê várias vezes.",
+    "nome do grupo de apoio que hazel frequenta.",
+    "lugar onde augustus e hazel trocam seu primeiro beijo.",
+
+];
